164d229c7ea9370c26fa2483ee08bf57ba2a40c4
Ibrahim Soltan <ibrahim.soltan918@gmail.com>
2022-12-25 14:33:51 +0200

    feat: Rep can view upcoming matches.

---
+++ ASP_Web_App/stadium_tickets_system/stadium_tickets_system/UpcomingMatches.aspx
@@ -0,0 +1,66 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="UpcomingMatches.aspx.cs" Inherits="stadium_tickets_system.UpcomingMatches" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Upcoming Matches</h1>
+            <h2>Upcoming Matches that is assigned to a Stadium</h2>
+            <asp:Table ID="upcomingMatchesTableWithStadium" runat="server">
+                <asp:TableHeaderRow>
+                    <asp:TableHeaderCell>
+                        Host Club
+                    </asp:TableHeaderCell>
+
+                    <asp:TableHeaderCell>
+                        Guest Club
+                    </asp:TableHeaderCell>
+
+                    <asp:TableHeaderCell>
+                        Start Time
+                    </asp:TableHeaderCell>
+
+
+                    <asp:TableHeaderCell>
+                        End Time
+                    </asp:TableHeaderCell>
+
+                    <asp:TableHeaderCell>
+                        Stadium Name
+                    </asp:TableHeaderCell>
+                </asp:TableHeaderRow>
+                
+            </asp:Table>
+
+            <h2>Upcoming Matches that is not assigned to a Stadium</h2>
+            <asp:Table ID="upcomingMatchesTableWithoutStadium" runat="server">
+                <asp:TableHeaderRow>
+                    <asp:TableHeaderCell>
+                        Host Club
+                    </asp:TableHeaderCell>
+
+                    <asp:TableHeaderCell>
+                        Guest Club
+                    </asp:TableHeaderCell>
+
+                    <asp:TableHeaderCell>
+                        Start Time
+                    </asp:TableHeaderCell>
+
+
+                    <asp:TableHeaderCell>
+                        End Time
+                    </asp:TableHeaderCell>
+
+                </asp:TableHeaderRow>
+                
+            </asp:Table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ASP_Web_App/stadium_tickets_system/stadium_tickets_system/UpcomingMatches.aspx.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace stadium_tickets_system
+{
+    public partial class UpcomingMatches : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            String userid = "3";
+
+            string connStr = WebConfigurationManager.ConnectionStrings["StadiumDatabaseConn"].ToString();
+            SqlConnection conn = new SqlConnection(connStr);
+
+            SqlCommand upcomingMatchesWithStadium = new SqlCommand("readUpcomingMatchesWithStadium", conn);
+            upcomingMatchesWithStadium.CommandType = System.Data.CommandType.StoredProcedure;
+            upcomingMatchesWithStadium.Parameters.Add(new SqlParameter("@rep_id", userid));
+
+            conn.Open();
+            SqlDataReader rdr = upcomingMatchesWithStadium.ExecuteReader(System.Data.CommandBehavior.CloseConnection);
+            
+
+            while (rdr.Read())
+            {
+                TableRow match = new TableRow();
+
+                TableCell hostClub = new TableCell();
+                match.Cells.Add(hostClub);
+                hostClub.Text = rdr.GetString(rdr.GetOrdinal("Host"));
+
+                TableCell guestClub = new TableCell();
+                match.Cells.Add(guestClub);
+                guestClub.Text = rdr.GetString(rdr.GetOrdinal("Guest"));
+
+                TableCell startTime = new TableCell();
+                match.Cells.Add(startTime);
+                startTime.Text = rdr.GetDateTime(rdr.GetOrdinal("startTime")).ToString();
+
+                TableCell endTime = new TableCell();
+                match.Cells.Add(endTime);
+                endTime.Text = rdr.GetDateTime(rdr.GetOrdinal("endTime")).ToString();
+
+
+                TableCell StadiumName = new TableCell();
+                match.Cells.Add(StadiumName);
+                StadiumName.Text = rdr.GetString(rdr.GetOrdinal("StadiumName"));
+
+                upcomingMatchesTableWithStadium.Rows.Add(match);
+
+            }
+            
+            conn.Close();
+            conn.Open();
+
+            SqlCommand upcomingMatchesWithoutStadium = new SqlCommand("readUpcomingMatchesWithoutStadium", conn);
+            upcomingMatchesWithoutStadium.CommandType = System.Data.CommandType.StoredProcedure;
+            upcomingMatchesWithoutStadium.Parameters.Add(new SqlParameter("@rep_id", userid));
+
+            rdr = upcomingMatchesWithoutStadium.ExecuteReader(System.Data.CommandBehavior.CloseConnection);
+
+
+            while (rdr.Read())
+            {
+                TableRow match = new TableRow();
+
+                TableCell hostClub = new TableCell();
+                match.Cells.Add(hostClub);
+                hostClub.Text = rdr.GetString(rdr.GetOrdinal("Host"));
+
+                TableCell guestClub = new TableCell();
+                match.Cells.Add(guestClub);
+                guestClub.Text = rdr.GetString(rdr.GetOrdinal("Guest"));
+
+                TableCell startTime = new TableCell();
+                match.Cells.Add(startTime);
+                startTime.Text = rdr.GetDateTime(rdr.GetOrdinal("startTime")).ToString();
+
+                TableCell endTime = new TableCell();
+                match.Cells.Add(endTime);
+                endTime.Text = rdr.GetDateTime(rdr.GetOrdinal("endTime")).ToString();
+
+                upcomingMatchesTableWithoutStadium.Rows.Add(match);
+
+            }
+
+            conn.Close();
+        }
+    }
+}
